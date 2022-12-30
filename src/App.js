58e53faea3ff32ede6c3aff2a4549af6b27b4636
eltirolibre <109189557+eltirolibre@users.noreply.github.com>
2022-12-30 18:04:41 -0500

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      Weather App.
+      <footer>
+        This app was coded by MP and is available {""}
+        <a
+          href="https://github.com/eltirolibre/react-weather-mp"
+          target="_blank"
+        >
+          on Github
+        </a>
+        .
+      </footer>
     </div>
   );
 }
