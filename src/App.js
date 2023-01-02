7c59daa6492536c491cd424baf8a95d3c5c6712f
eltirolibre <109189557+eltirolibre@users.noreply.github.com>
2023-01-02 14:32:01 -0500

    Added + imported Boostrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App.
-      <footer>
-        This app was coded by MP and is available {""}
-        <a
-          href="https://github.com/eltirolibre/react-weather-mp"
-          target="_blank"
-        >
-          on Github
-        </a>
-        .
-      </footer>
+      <div className="container">
+        Weather App.
+        <footer>
+          This app was coded by MP and is available {""}
+          <a
+            href="https://github.com/eltirolibre/react-weather-mp"
+            target="_blank"
+          >
+            on Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
