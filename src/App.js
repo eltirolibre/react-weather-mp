3b189e18115ee1c97e5e01aee021996bc170e230
eltirolibre <109189557+eltirolibre@users.noreply.github.com>
2023-01-08 12:25:08 -0500

    Updated App.js
    An attempt to troubleshoot the Netlify error

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Weather defaultCity="Toronto" />
         <footer>
-          This app was coded by MP and is available {""}
+          This app was coded by MP. It is available {""}
           <a
             href="https://github.com/eltirolibre/react-weather-mp"
             target="_blank"
+            rel="noopener noreferrer"
           >
             on Github
+          </a>{" "}
+          and hosted{" "}
+          <a
+            href="https://boisterous-mousse-8ad5a2.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            on Netflify
           </a>
           .
         </footer>
@@ -21,5 +31,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
